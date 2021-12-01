94e667b337d800f0fe0da3812866fd64fd4739db
Nianchen Shen <Nianchen.Shen@Outlook.com>
2021-12-01 18:02:41 +0800

    feat(idm,timer): add a simple timer to stop download function after certain time

--- src/idm.py
+++ src/idm.py
@@ -1,25 +1,17 @@
 import comtypes.client as cc
 import comtypes
 import logging
+import timer
 import urllib.request as dl
 import multiprocessing
 import time
 
 
-def tick_tok(target: int):
-    for i in range(target):
-        print('time elapsed: ' + str(i) + 'sec')
-        time.sleep(1)
-
-
+@timer.exit_after(10)
 def download_test(url: str):
-    dl.urlretrieve(url, './download/test.flv')
+    dl.urlretrieve(url, '../download/test.flv')
 
 
 if __name__ == '__main__':
     url = 'http://dyscdnali1.douyucdn.cn/live/288016rlols5.flv?uuid='
-    p = multiprocessing.Process(target=download_test(url))
-    p.start()
-    time.sleep(15)
-    p.terminate()
-    print('Download Process Terminated')
+    download_test(url)
