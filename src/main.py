f0bacb401b2a4df5703b9d878f24e635c6830ed8
Nianchen Shen <Nianchen.Shen@Outlook.com>
2021-12-02 16:03:22 +0800

    feat(main): add more detailed timestamp and duration prompt setting
    1.add mins in filename timestamp to prevent deplication
    2.ask for duration input

--- src/main.py
+++ src/main.py
@@ -6,21 +6,28 @@ import datetime
 
 def main():
     room_id = input('Type room id(douyu.com): \n')
+    dur = input('Set downloading/recording duration(in sec): \n')
+    dur = int(dur)
+    idm.timeout = dur
     s = douyu.DouYu(room_id)
     url = s.get_real_url()['flv']
     dest = '../download/'
     filename = \
-        dest + datetime.datetime.now().strftime('%Y-%m-%d_%H_') + 'DouYu_' + \
+        dest + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M_') + 'DouYu_'\
+        + \
         str(room_id)
     filename_before = filename + '.flv'
     filename_after = filename + '.mp4'
     dm = idm.download_wrapper(url, filename_before)
     try:
+        print('Start downloading/recording... Duration set to: ' + str(
+            idm.timeout) + 's')
         dm.download()
     except KeyboardInterrupt:
         print('Download Completed')
         me = transcode.transcode_wrapper(filename_before, filename_after)
         me.transcode()
+        print('Filename after transcode: ' + filename_after)
 
 
 if __name__ == '__main__':
