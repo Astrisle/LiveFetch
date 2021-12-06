14268abd1e5030074054a43f4746e99e0a5da6f1
Nianchen Shen <Nianchen.Shen@Outlook.com>
2021-12-06 11:01:24 +0800

    docs(config): add comments for config

--- src/config.py
+++ src/config.py
@@ -1,18 +1,11 @@
 # Recoding Duration / 录制时长
-duration = 7800
+duration = 10
 
 # Shutdown PC after recording & transcoding complete / 完成后自动关机
-auto_shutdown = True
+auto_shutdown = False
 
+# Retry timeout for source url resolve / 解析视频源地址失败重试等待时间
 resolve_retry_timeout = 15
 
+# Retry timeout for download / 下载失败(多见于直播流尚未到达CDN导致源地址无效)重试等待时间
 download_retry_timeout = 15
-
-# class dl_config(object):
-#     def __init__(self):
-#         self.config = {
-#             'duration': 300
-#         }
-#
-#     def set_config_duration(self, dur: int):
-#         self.config['duration'] = dur
