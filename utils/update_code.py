affbdbcccc8ce666d1cd0ec201475c4b7ffce832
Nianchen Shen <Nianchen.Shen@Outlook.com>
2021-12-16 10:12:56 +0800

    feat(update_code): completed(for now)

--- utils/update_code.py
+++ utils/update_code.py
@@ -1,8 +1,9 @@
-import requests
 import json
-from pathlib import Path
 import os
-import threading
+import time
+
+import requests
+import logging
 
 
 def get_gift_codes():
@@ -53,94 +54,106 @@ def get_gift_codes():
     gift_all_dict = cash_gift_dict | gift_dict
 
     # compare if name from props dict vary from effects
-    # TODO - validate if this step is necessary
-    res = {}
-    res_vary = {}
+    # TODO - validate if this step is necessary - Its not
+    # res = {}
+    # res_vary = {}
+    #
+    # for gift in gift_all_dict:
+    #     code = gift_all_dict[gift]['code']
+    #     name = gift_all_dict[gift]['name']
+    #     exp = gift_all_dict[gift]['exp']
+    #     code_effect = gift_all_dict[gift]['effect_code']
+    #     if code_effect != 0:
+    #         name_from_effect = gift_effects_dict[str(code_effect)][
+    #             'effect_name']
+    #     else:
+    #         name_from_effect = 'No Effects'
+    #
+    #     # reconstruct
+    #     if name == name_from_effect or name_from_effect == 'No Effects':
+    #         res[gift] = {
+    #             'code': code,
+    #             'name': name,
+    #             'exp': exp,
+    #             'effect_code': code_effect,
+    #             'effect_name': name_from_effect
+    #         }
+    #     else:
+    #         res_vary[gift] = {
+    #             'code': code,
+    #             'name': name,
+    #             'exp': exp,
+    #             'effect_code': code_effect,
+    #             'effect_name': name_from_effect
+    #         }
 
-    for gift in gift_all_dict:
-        code = gift_all_dict[gift]['code']
-        name = gift_all_dict[gift]['name']
-        exp = gift_all_dict[gift]['exp']
-        code_effect = gift_all_dict[gift]['effect_code']
-        if code_effect != 0:
-            name_from_effect = gift_effects_dict[str(code_effect)][
-                'effect_name']
-        else:
-            name_from_effect = 'No Effects'
-
-        # reconstruct
-        if name == name_from_effect or name_from_effect == 'No Effects':
-            res[gift] = {
-                'code': code,
-                'name': name,
-                'exp': exp,
-                'effect_code': code_effect,
-                'effect_name': name_from_effect
-            }
-        else:
-            res_vary[gift] = {
-                'code': code,
-                'name': name,
-                'exp': exp,
-                'effect_code': code_effect,
-                'effect_name': name_from_effect
-            }
-
-    return res, res_vary
+    return gift_all_dict
 
 
-def construct_config_file(res: tuple, path: str):
+def construct_config_file(res: dict, path: str):
+    # BELOW IS DEPRECATED - Not Necessary
     # read original config file
-    raw = Path(path).read_text()
-    split_raw = raw.splitlines()
+    # raw = Path(path).read_text()
+    # split_raw = raw.splitlines()
+    #
+    # # initialise array&dict instance
+    # original = {}
+    # arr_code = []
+    # arr_name = []
+    # arr_exp = []
+    #
+    # for txt in split_raw:
+    #     single = txt.split(',')
+    #     arr_code.append(single[0])
+    #     arr_name.append(single[1])
+    #     arr_exp.append(single[2])
+    #
+    # # reconstruct dict from raw for comparison
+    # if all(length == len(arr_code) for length in [len(arr_name), len(arr_exp)]):
+    #     for i in range(len(arr_code)):
+    #         original[str(arr_code[i])] = {
+    #             'code': arr_code[i],
+    #             'name': arr_name[i],
+    #             'exp': arr_exp[i]
+    #         }
+    # else:
+    #     raise RuntimeError('原配置文件输入数组长度不一致')
+    #
+    # # retrieve from tuple index 0
+    # fetched_gifts = res
+    #
+    # # iterate through local config to see if there is a mismatch
+    # # if there is, move conflict items to a new dict
+    # conflict = {}
+    # for gift in original:
+    #     if gift in fetched_gifts:
+    #         if original[gift]['code'] == fetched_gifts[gift]['code'] and \
+    #                 original[gift]['name'] != fetched_gifts[gift]['name']:
+    #             conflict[gift] = {
+    #                 'local': original[gift],
+    #                 'remote': fetched_gifts[gift]
+    #             }
+    #             del original[gift]
+    #             del fetched_gifts[gift]
+    #
+    # # if len(conflict) == 0:
+    #
+    # print('test')
 
-    # initialise array&dict instance
-    original = {}
-    arr_code = []
-    arr_name = []
-    arr_exp = []
-
-    for txt in split_raw:
-        single = txt.split(',')
-        arr_code.append(single[0])
-        arr_name.append(single[1])
-        arr_exp.append(single[2])
-
-    # reconstruct dict from raw for comparison
-    if all(length == len(arr_code) for length in [len(arr_name), len(arr_exp)]):
-        for i in range(len(arr_code)):
-            original[str(arr_code[i])] = {
-                'code': arr_code[i],
-                'name': arr_name[i],
-                'exp': arr_exp[i]
-            }
-    else:
-        raise RuntimeError('原配置文件输入数组长度不一致')
-
-    # retrieve from tuple index 0
-    fetched_gifts = res[0]
-
-    # iterate through local config to see if there is a mismatch
-    # if there is, move conflict items to a new dict
-    conflict = {}
-    for gift in original:
-        if gift in fetched_gifts:
-            if original[gift]['code'] == fetched_gifts[gift]['code'] and \
-                    original[gift]['name'] != fetched_gifts[gift]['name']:
-                conflict[gift] = {
-                    'local': original[gift],
-                    'remote': fetched_gifts[gift]
-                }
-                del original[gift]
-                del fetched_gifts[gift]
-
-    # if len(conflict) == 0:
-
-    print('test')
-
-    with open('test_text/codes.txt', 'w') as file:
-        file.write('test')
+    with open(path, 'w') as file:
+        for gift in res:
+            singe_str = str(res[gift]['code']) + ',' + str(
+                res[gift]['name']) \
+                        + ',' \
+                        + str(res[gift]['exp'])
+            file.write(singe_str + '\n')
+        os.system('msg %username% 成功导入了' + str(len(res)) + '条数据')
 
 
 if __name__ == '__main__':
-    construct_config_file(get_gift_codes(), 'test_text/raw.txt')
+    try:
+        construct_config_file(get_gift_codes(), '../giftConfig.dat')
+    except Exception as e:
+        os.system('msg %username% 导入过程中出现异常,请截图反馈')
+        logging.error(e)
+        os.system('timeout /t -1')
